ceeade875cb35074e963d1286bcc99883f79d0cd
jime <jimemiramontes@gmail.com>
2024-09-01 21:01:39 -0300

    server

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, { json } from "express";
+
+//import errorHandler from "./middlewares/error-handler"
+
+//import indexRouter from "./routes"
+
+const app = express();
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+import { config } from "dotenv";
+
+config();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log("Server listening on PORT", PORT));

e23db0a984be969c039e5019034956bea412b9ea
jime <jimemiramontes@gmail.com>
2024-09-07 00:17:33 -0300

    routes updeted

--- src/routes/players.ts
+++ src/routes/players.ts
@@ -3,8 +3,8 @@ import PlayersController from "../controllers/players";
 const playersRouter = Router();
 
 playersRouter.get("/", PlayersController.getByFilters); //ok
-//playersRouter.get("/:id", PlayersController.getById); //ok
-playersRouter.get("/:name", PlayersController.getByName); //-------no anda xq entra a getById
+playersRouter.get("/:id", PlayersController.getById); //ok
+//playersRouter.get("/:name", PlayersController.getByName); //-------no anda xq entra a getById
 playersRouter.patch("/:id", PlayersController.updateById); //ok
 playersRouter.post("/", PlayersController.create); //ok
 playersRouter.delete("/:id", PlayersController.deleteById); //ok
